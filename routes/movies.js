d762e2cea16391fe70a2c6894a0a668ed54ee610
andreizherebor <115610356+andreizherebor@users.noreply.github.com>
2023-07-06 15:29:24 +0700

    Revert "Level 1"

--- routes/movies.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
-const { validationCreateMovie, validationDeleteMovie } = require('../middlewares/validations');
-
-router.get('/movies', getMovies);
-router.post('/movies', validationCreateMovie, createMovie);
-router.delete('/movies/:movieId', validationDeleteMovie, deleteMovie);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getUser, updateUser } = require('../controllers/users');
-const { validationGetUser, validationUpdateUser } = require('../middlewares/validations');
-
-router.get('/users/me', validationGetUser, getUser);
-router.patch('/users/me', validationUpdateUser, updateUser);
-
-module.exports = router;
